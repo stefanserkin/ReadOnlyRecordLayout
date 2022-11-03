9812f3b4090dc80d50d7dee528c3a319f0e47df6
Stefan Serkin <stefanserkin@gmail.com>
2022-11-03 13:31:55 -0400

    record form updates

--- force-app/main/default/lwc/journalEntryDetailsReadOnly/journalEntryDetailsReadOnly.js
+++ force-app/main/default/lwc/journalEntryDetailsReadOnly/journalEntryDetailsReadOnly.js
@@ -1,5 +1,4 @@
 import { LightningElement, api } from 'lwc';
-//import NAME_FIELD from '@salesforce/schema/Contact.Name';
 
 export default class JournalEntryDetailsReadOnly extends LightningElement {
     @api recordId;
